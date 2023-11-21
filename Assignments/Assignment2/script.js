fb10a4623f065b01579142d1fd2f0934322fa1f3
ujjwal <ujjwalpratapsingh01@gmail.com>
2023-11-21 23:45:40 +0530

    Assignment2

---
+++ Assignments/Assignment2/script.js
@@ -0,0 +1,22 @@
+
+function add(){
+    let x=parseFloat(document.getElementById("num1").value);
+let y=parseFloat(document.getElementById("num2").value);
+document.getElementById("result").innerHTML=x+y;
+}
+function mul(){
+    let x=parseFloat(document.getElementById("num1").value);
+let y=parseFloat(document.getElementById("num2").value);
+document.getElementById("result").innerHTML=x*y;
+}
+function sub(){
+    let x=parseFloat(document.getElementById("num1").value);
+let y=parseFloat(document.getElementById("num2").value);
+document.getElementById("result").innerHTML=x-y;
+}
+function div(){
+    let x=parseFloat(document.getElementById("num1").value);
+let y=parseFloat(document.getElementById("num2").value);
+document.getElementById("result").innerHTML=x/y;
+}
+
